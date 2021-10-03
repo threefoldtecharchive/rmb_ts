0478bc0b651b872c2a05de1ef3595ea5a457dc0f
dmahmouali <dmahmou213@gmail.com>
2021-10-03 18:23:32 +0200

    init commit

---
+++ scripts/softTest.ts
@@ -0,0 +1,31 @@
+
+import { HTTPMessageBusClient } from "../src/httpClient"
+
+
+
+function delay(s: number) {
+    return new Promise( resolve => setTimeout(resolve, s*1000) );
+}
+
+async function main() {
+    const dstNodeId = 4;
+
+    async function deploy() {
+
+        let rmb = new HTTPMessageBusClient("https://rmbproxy1.devnet.grid.tf");
+        let msg = rmb.prepare("zos.statistics.get", [dstNodeId], 0, 2);
+        console.log(msg)
+        const retMsg = await rmb.send(msg, "{'test':'test'}")
+        console.log(retMsg)
+        
+        // set the retqueue to oringnal message
+        await delay(7);
+
+        const result = await rmb.read(retMsg);
+        console.log(`the read response is: ${JSON.stringify(result)}`);
+    }
+
+    deploy()
+}
+
+main()
