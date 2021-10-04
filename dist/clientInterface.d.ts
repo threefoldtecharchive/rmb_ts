3ea26dc7ff44ec81adc84fb5dd57d08dae60ff77
MElborolossy <mohammedelborolossy@gmail.com>
2021-10-04 18:42:16 +0200

    Update interface

--- dist/clientInterface.d.ts
+++ dist/clientInterface.d.ts
@@ -1,7 +1,7 @@
 interface MessageBusClientInterface {
     client: any;
-    prepare(command: string, destination: string, expiration: number, retry: number): object;
-    send(message: object, payload: object): object;
-    read(message: object): object;
+    prepare(command: string, destination: string[], expiration: number, retry: number): Record<string, unknown>;
+    send(message: Record<string, unknown>, payload: string): Record<string, unknown>;
+    read(message: Record<string, unknown>): Record<string, unknown>;
 }
 export { MessageBusClientInterface };
--- src/clientInterface.ts
+++ src/clientInterface.ts
@@ -1,8 +1,8 @@
 interface MessageBusClientInterface {
     client: any;
-    prepare(command: string, destination: string, expiration: number, retry: number): object;
-    send(message: object, payload: object): object;
-    read(message: object): object;
+    prepare(command: string, destination: string[], expiration: number, retry: number): Record<string, unknown>;
+    send(message: Record<string, unknown>, payload: string): Record<string, unknown>;
+    read(message: Record<string, unknown>): Record<string, unknown>;
 }
 
 export { MessageBusClientInterface };
