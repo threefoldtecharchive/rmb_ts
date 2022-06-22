e8c122a77bc9a0741834bbab96b2fe1c28447eb4
Abdelrahman Ghanem <abom.jdev@gmail.com>
2022-06-22 14:31:19 +0200

    force verifying response

--- packages/http_client/scripts/softTest.ts
+++ packages/http_client/scripts/softTest.ts
@@ -9,13 +9,14 @@ async function main() {
 
     async function deploy() {
         const rmb = new HTTPMessageBusClient(133, "http://localhost:8055", "https://graphql.dev.grid.tf/graphql", "behave element congress universe grit door reform noise fringe relax shop analyst");
+        rmb.verifyResponse = true;
         const msg = rmb.prepare("zos.statistics.get", [dstNodeId], 1000, 2);
         const retMsg = await rmb.send(msg, "{'test':'test'}");
         // set the retqueue to oringnal message
         await delay(3);
 
         const result = await rmb.read(retMsg);
-        console.log(`the read response is:`);
+        console.log(`the read verified response is:`);
         console.log(result);
     }
 
