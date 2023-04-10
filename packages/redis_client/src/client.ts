cc6426701a9107a0bdf250852667161e5a29cd6a
ahmedhanafy725 <hanafya@incubaid.com>
2023-04-10 14:51:17 +0200

    Update redis client to be compatible with rmb-peer

--- packages/redis_client/src/client.ts
+++ packages/redis_client/src/client.ts
@@ -1,7 +1,8 @@
 import { createClient, RedisClientType } from "redis";
 import uuid4 from "uuid4";
-import { MessageBusClientInterface, Message } from "ts-rmb-client-base";
-class MessageBusClient implements MessageBusClientInterface {
+import { JsonIncomingResponse, JsonOutgoingRequest } from "./types"
+
+class MessageBusClient {
     client: RedisClientType
     constructor(port = 6379) {
         this.client = createClient({
@@ -9,52 +10,49 @@ class MessageBusClient implements MessageBusClientInterface {
                 port: port,
             },
         });
+        this.connect()
+    }
+
+    async connect() {
         this.client.connect()
-        this.client.on("error", function (error) {
-            console.error(error);
-        });
     }
 
-    prepare(command: string, destination: number[], expiration: number, retry: number): Message {
-        return {
+    async disconnect() {
+        this.client.disconnect()
+    }
+
+    async send(requestCommand: string, requestData: string, destinationTwinId: number, expirationMinutes: number): Promise<string> {
+        const uuid = uuid4()
+        const outgoingRequest: JsonOutgoingRequest = {
             ver: 1,
-            uid: "",
-            cmd: command,
-            exp: expiration,
-            dat: "",
-            src: 0,
-            dst: destination,
-            ret: uuid4(),
-            try: retry,
-            shm: "",
+            ref: uuid,
+            cmd: requestCommand,
+            exp: expirationMinutes,
+            dat: Buffer.from(requestData).toString("base64"),
+            tag: "",
+            dst: [destinationTwinId],
+            ret: uuid,
+            shm: "application/json",
             now: Math.floor(new Date().getTime() / 1000),
-            err: "",
-            sig: "",
-            pxy: false
-        };
-    }
-
-    async send(message: Message, payload: string): Promise<Message> {
-        const buffer = Buffer.from(payload);
-        message.dat = buffer.toString("base64");
-        const request = JSON.stringify(message);
-        await this.client.lPush("msgbus.system.local", request);
-        console.log(request);
-        return message;
-    }
-
-    async read(message: Message): Promise<Message[]> {
-        console.log("waiting reply", message.ret);
-        const responses = [];
-        for (let i = 0; i < message.dst.length; i++) {
-            const res = await this.client.blPop(message.ret, 0)
-            if (res) {
-                const response = JSON.parse(res.element);
-                response.dat = Buffer.from(response.dat, "base64").toString("ascii");
-                responses.push(response);
-            }
         }
-        return responses
+
+        const request = JSON.stringify(outgoingRequest);
+        await this.client.lPush("msgbus.system.local", request);
+        return outgoingRequest.ref;
+    }
+
+    async read(requestId: string, timeoutMinutes = 1): Promise<unknown> {
+        const res = await this.client.blPop(requestId, timeoutMinutes * 60)
+
+        if (res) {
+            const incomingResponse: JsonIncomingResponse = JSON.parse(res.element)
+            if (incomingResponse.err) {
+                throw Error(`Failed to read response due to: ${incomingResponse.err.message}`)
+            }
+            const data = Buffer.from(incomingResponse.dat, "base64").toString("ascii")
+            return JSON.parse(data)
+        }
+        throw Error(`Couldn't get the response in ${timeoutMinutes} minutes`)
     }
 }
 
